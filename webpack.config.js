da0b0d8efc529f90e2e6601541c679d9b9d13a08
Jennie-Watson <watson.jennie17@gmail.com>
2019-08-29 12:04:40 +1200

    commented out webpack to run properly

--- webpack.config.js
+++ webpack.config.js
@@ -19,14 +19,14 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       }]
   },
-  test: /\.(png|jp(e*)g|svg)$/,
-  use: [{
-    loader: 'url-loader',
-    options: {
-      limit: 8000, // Convert images < 8kb to base64 strings
-      name: 'images/[hash]-[name].[ext]'
-    }
-  }],
+  // test: /\.(png|jp(e*)g|svg)$/,
+  // use: [{
+  //   loader: 'url-loader',
+  //   options: {
+  //     limit: 8000, // Convert images < 8kb to base64 strings
+  //     name: 'images/[hash]-[name].[ext]'
+  //   }
+  // }],
   resolve: {
     extensions: ['.js', '.jsx']
   },
